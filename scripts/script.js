edebc4e3b87f43226b478067836ae4f3786036b0
PENN JR <pennjunior680@gmail.com>
2024-08-15 11:21:30 +0100

    Making a fetch request to the quote api

---
+++ scripts/script.js
@@ -0,0 +1,54 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let apiQuotes = [];
+
+//Show New Quote
+function newQuotes() {
+    //Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    //Check if Author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    //Check Quote length to determine styling
+    if (quote.quote.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.quote;
+    console.log(quote);
+}
+
+//Get Quotes from API
+async function getQuotes() {
+    const apiUrl = 'https://dummyjson.com/quotes?limit=1000';
+    try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        apiQuotes = data.quotes;
+        newQuotes();
+    } catch (err) {
+        //Catch errror here
+        console.log('Whoops, no quote', err);
+    }
+}
+
+//Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', newQuotes);
+twitterBtn.addEventListener('click', tweetQuote);
+
+//On Load
+getQuotes();
