a205f2a5024a637012236513a6b05e97aafb7d01
PENN JR <pennjunior680@gmail.com>
2024-08-15 16:22:25 +0100

    Adding the loader for fetching quotes

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+//Loading Spinner Shown
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Remove Loading Spinner
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 //Show New Quote
 function newQuotes() {
+    loading();
     //Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     //Check if Author field is blank and replace it with 'Unknown'
@@ -22,12 +36,15 @@ function newQuotes() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    //Set Quote, Hide Loader
     quoteText.textContent = quote.quote;
+    complete();
     console.log(quote);
 }
 
 //Get Quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://dummyjson.com/quotes?limit=1000';
     try {
         const res = await fetch(apiUrl);
